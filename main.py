41f8130da1448d7e9999b13eed1150fbb6140585
jackspock <lauty3298@gmail.com>
2026-06-17 14:56:15 -0300

    rich añadido para mejor ejemplo

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from colorama import init, Fore
+from rich.console import Console
+from rich.panel import Panel
 
 init(strip=False)
 
@@ -13,3 +15,20 @@ match color:
         print(Fore.BLUE + "El color es azul")
     case _:
         print("Color no reconocido")
+
+console = Console()
+
+def main():
+    console.print("\n[bold green]¡Hola desde Docker![/bold green] :rocket:")
+    
+    console.print(
+        Panel.fit(
+            "[bold cyan]Rich[/bold cyan] está funcionando correctamente dentro de tu contenedor.",
+            title="[bold magenta]Éxito[/bold magenta]",
+            border_style="green"
+        )
+    )
+    print("") # Un espacio en blanco al final
+
+if __name__ == "__main__":
+    main()
